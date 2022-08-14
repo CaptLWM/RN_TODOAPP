cddfb310a813f0ede5541f779051c42eb073e18a
CaptLWM <wonmin275@gmail.com>
2022-08-14 21:31:46 +0900

    RN 3.3 todo 1

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppToDo from './components/AppToDo';
 import DateHead from './components/DateHead';
@@ -12,11 +19,16 @@ const App = () => {
     // <SafeAreaView style={{flex:1}}> ios 노치 양옆 채울때
     <SafeAreaProvider>
       <SafeAreaView style={styles.block}>
-        {/* SafeAreaView는 언제나 SafeAreaProvider 안에 */}
-        {/* edge => top, left, right, bottom / left right는 가로모드사용할 때 필요함 */}
-        <DateHead date={today} />
-        <Empty />
-        <AppToDo />
+        <KeyboardAvoidingView
+          behavior={Platform.select({ios: 'padding', android: undefined})}
+          //ios면 keyboard가 올라오면서 input도 밀어버리게(화면 가리지 않게)
+          style={styles.avoid}>
+          {/* SafeAreaView는 언제나 SafeAreaProvider 안에 */}
+          {/* edge => top, left, right, bottom / left right는 가로모드사용할 때 필요함 */}
+          <DateHead date={today} />
+          <Empty />
+          <AppToDo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -27,6 +39,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     //안드로이드 배경색은 기본이 연한 회색
+  },
+  avoid: {
+    flex: 1,
   },
 });
 export default App;
--- components/AppToDo.js
+++ components/AppToDo.js
@@ -1,14 +1,109 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 const AppToDo = () => {
-  return <View style={styles.block} />;
+  const [text, setText] = React.useState('');
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+        //enter누르면 호출되는 함수
+        returnKeyType="done"
+        // enter의 타입을 지정, enter 부분에 보이는 설명 또는 아이콘 변경됨
+      />
+      {/* {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        // ios에서 사용 불가
+        <View style={styles.circleWrapper}>
+          <TouchableNativeFeedback>
+            <View style={styles.buttonStyle}>
+              <Image
+                source={require('../assets/icons/add_white/add_white.png')}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      )} */}
+      {/* 짧게 리팩토링 */}
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
+    // 효과가 영역 바깥까지 나오는거 방지
   },
 });
+
 export default AppToDo;
